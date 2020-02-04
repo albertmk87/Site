11cd2a30f8a3bc088cd96acc5208931b03f19424
albertmk87 <albertmk87@gmail.com>
2020-02-04 13:00:52 +0100

    sticky-header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,10 @@
 import mobileMenu from "./modules/mobileMenu";
 import revealOnScroll from "./modules/revealOnScroll";
+import stickyHeader from "./modules/stickyHeader";
 import $ from "jquery"
 
 var mobileMenu1=new mobileMenu();
 var revealOnScroll1=new revealOnScroll($(".feauture-item"),"85%");
 var revealOnScroll2=new revealOnScroll($(".testimonial"),"60%");
+
+var stickyHeader1=new stickyHeader();
