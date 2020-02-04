7ad3a1c6ced45018bfdb884d463d5eb66407e5bf
albertmk87 <albertmk87@gmail.com>
2020-02-04 11:09:52 +0100

    Added reveal on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
 import mobileMenu from "./modules/mobileMenu";
+import revealOnScroll from "./modules/revealOnScroll";
+import $ from "jquery"
 
 var mobileMenu1=new mobileMenu();
+var revealOnScroll1=new revealOnScroll($(".feauture-item"),"85%");
+var revealOnScroll2=new revealOnScroll($(".testimonial"),"60%");
